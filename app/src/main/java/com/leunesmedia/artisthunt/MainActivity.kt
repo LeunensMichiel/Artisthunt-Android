33b26d243a97a65f43918f772ed86efb597bde90
Michiel Leunens <michiel.leunens.y7743@student.hogent.be>
2018-12-18 16:13:51 +0100

    Login and Register UI App Icon Theme Update

--- app/src/main/java/com/leunesmedia/artisthunt/MainActivity.kt
+++ app/src/main/java/com/leunesmedia/artisthunt/MainActivity.kt
@@ -3,9 +3,14 @@ package com.leunesmedia.artisthunt
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.leunesmedia.artisthunt.user.LoginFragment
+import com.leunesmedia.artisthunt.user.RegisterFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loginFragment: LoginFragment
+    private lateinit var registerFragment: RegisterFragment
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -28,4 +33,30 @@ class MainActivity : AppCompatActivity() {
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+    fun toLogin(v: View) {
+        loginFragment = LoginFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_activity_frame, loginFragment)
+            .commit()
+    }
+
+    /**
+     * This function replaces the login fragment with the register fragment
+     */
+    fun toRegister(v: View) {
+        registerFragment = RegisterFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_activity_frame, registerFragment)
+            .commit()
+    }
+    /**
+     * This function replaces the login fragment with the forgotPassword fragment
+     */
+//    fun toForgot(v : View) {
+//        forgotPasswordFragment = ForgotPasswordFragment()
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.session_container, forgotPasswordFragment)
+//            .commit()
+//    }
 }
